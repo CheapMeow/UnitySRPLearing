199b70f3f5a88c71ab01dd22da91e8f7072a2e30
CheapMeow <blizzzzzzzzzzzzard@gmail.com>
2025-02-15 18:55:13 +0800

    3.6 Cutoff Per Object

--- Assets/Custom RP/Runtime/PerObjectMaterialProperties.cs
+++ Assets/Custom RP/Runtime/PerObjectMaterialProperties.cs
@@ -5,10 +5,14 @@ public class PerObjectMaterialProperties : MonoBehaviour
 {
 
     static int baseColorId = Shader.PropertyToID("_BaseColor");
-
+    static int cutoffId = Shader.PropertyToID("_Cutoff");
+    
     [SerializeField]
     Color baseColor = Color.white;
 
+    [SerializeField, Range(0f, 1f)]
+    float cutoff = 0.5f;
+    
     static MaterialPropertyBlock block;
 
     void Awake()
@@ -23,6 +27,7 @@ public class PerObjectMaterialProperties : MonoBehaviour
             block = new MaterialPropertyBlock();
         }
         block.SetColor(baseColorId, baseColor);
+        block.SetFloat(cutoffId, cutoff);
         GetComponent<Renderer>().SetPropertyBlock(block);
     }
 }
